8dad80b9bab4c044667914c45b9c37d36d061367
Muxammadbobur <bobur0172@gmail.com>
2023-03-17 05:38:19 -0700

    first commit

--- src/containers/Chart/Chart.tsx
+++ src/containers/Chart/Chart.tsx
@@ -71,7 +71,7 @@ const Chart = () => {
       price: price,
     };
 
-    await fetch("https://various-roan-bean.glitch.me/pay", {
+    await fetch("http://emilkhairullin.ru:8000/pay", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
